908ff4ec949d2cb467b4eb32fa383114bc6032d1
felipe macedo <felipe.m4cedo@gmail.com>
2018-07-04 17:18:12 -0300

    commit inicial

---
+++ listavip/src/main/java/br/com/alura/listavip/Configuracao.java
@@ -0,0 +1,16 @@
+package br.com.alura.listavip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Configuracao {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Configuracao.class, args);
+	}
+
+}
